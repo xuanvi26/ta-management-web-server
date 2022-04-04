5eb7a5a196602eeb70101d33e1871c84a52a6c30
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2022-04-04 15:28:57 -0400

    Finish login page

--- src/routes/session.js
+++ src/routes/session.js
@@ -2,87 +2,92 @@ const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
 const { checkLoginCredentials } = require.main.require(
-  "./src/services/session"
+	"./src/services/session"
 );
 const { checkAuthentication } = require.main.require(
-  "./src/utils/authentication"
+	"./src/utils/authentication"
 );
 const { response_type } = require.main.require("./src/response");
 
 const schema = Joi.object({
-  username: Joi.string().required(),
-  password: Joi.string().required(),
+	username: Joi.string().required(),
+	password: Joi.string().required(),
 }).required();
 
 router.get("/", (req, res) => {
-  if (req.session.authenticated) {
-    res.render("pages/landing/home", {
-      userTypes: req.session.user.userTypes,
-      username: req.session.user.username
-    });
-  } else {
-    res.render("pages/landing/home");
-  }
+	if (req.session.authenticated) {
+		res.render("pages/landing/home", {
+			userTypes: req.session.user.userTypes,
+			username: req.session.user.username,
+		});
+	} else {
+		res.render("pages/landing/home");
+	}
 });
 
 router.get("/login", (req, res) => {
-  if (req.session.authenticated) {
-    res.render("pages/landing/home", {
-      userTypes: req.session.user.userTypes,
-      username: req.session.user.username
-    });
-  } else {
-    res.render("pages/landing/login");
-  }
+	if (req.session.authenticated) {
+		res.render("pages/landing/home", {
+			userTypes: req.session.user.userTypes,
+			username: req.session.user.username,
+		});
+	} else {
+		res.render("pages/landing/login", { errors: [] });
+	}
 });
 
 router.post("/login", async (req, res) => {
-  if (req.session.authenticated) {
-    res.render("pages/landing/home", {
-      userTypes: req.session.user.userTypes,
-      username: req.session.user.username
-    });
-    return;
-  }
+	if (req.session.authenticated) {
+		res.render("pages/landing/home", {
+			userTypes: req.session.user.userTypes,
+			username: req.session.user.username,
+		});
+		return;
+	}
 
-  let { error, value } = schema.validate(req.body);
-  if (error) {
-    res.status(400).json({
-      response: response_type.BAD_REQUEST,
-      errors: error.details.map((detail) => detail.message),
-    });
-    return;
-  }
+	let { error, value } = schema.validate(req.body);
+	if (error) {
+		res.status(400).render("pages/landing/login", {
+			errors: error.details.map((detail) => detail.message),
+		});
+		return;
+	}
 
-  const { username, password } = value;
-  let authenticatedUser = await checkLoginCredentials(username, password);
-  if (authenticatedUser) {
-    req.session.authenticated = true;
-    req.session.user = { userTypes: authenticatedUser.userTypes, username: authenticatedUser.username };
-    res.render("pages/landing/home", {
-      userTypes: req.session.user.userTypes,
-      username: req.session.user.username
-    });
-  } else {
-    res.status(401).json({
-      response: response_type.AUTH,
-      errors: ["Failed login"],
-    });
-  }
+	const { username, password } = value;
+	let authenticatedUser = await checkLoginCredentials(username, password);
+	if (authenticatedUser) {
+		req.session.authenticated = true;
+		req.session.user = {
+			userTypes: authenticatedUser.userTypes,
+			username: authenticatedUser.username,
+		};
+		res.render("pages/landing/home", {
+			userTypes: req.session.user.userTypes,
+			username: req.session.user.username,
+		});
+	} else {
+		res.status(400).render("pages/landing/login", {
+			response: response_type.AUTH,
+			errors: ["Unable to authenticate user"],
+		});
+	}
 });
 
 router.post(
-  "/logout",
-  checkAuthentication((req, res) => {
-    req.session.destroy((err) => {
-      if (err) {
-        res.json({ errors: [err.message], response: response_type.AUTH });
-      } else {
-        res.render("pages/landing/home");
-        // res.json({ errors: [], response: response_type.OK });
-      }
-    });
-  })
+	"/logout",
+	checkAuthentication((req, res) => {
+		req.session.destroy((err) => {
+			if (err) {
+				res.json({
+					errors: [err.message],
+					response: response_type.AUTH,
+				});
+			} else {
+				res.render("pages/landing/home");
+				// res.json({ errors: [], response: response_type.OK });
+			}
+		});
+	})
 );
 
 module.exports = router;
