17a77b714cc56e90da82f7b99587896a83725940
fufunafu <fuanne_gm@hotmail.com>
2022-04-16 19:40:05 -0400

    added ta

--- src/models/ta/index.js
+++ src/models/ta/index.js
@@ -1,5 +1,6 @@
 const writer = require.main.require('./src/utils/writer');
 
+const reader = require.main.require('./src/utils/reader');
 const TA_TABLE = './src/models/ta/db.json';
 const COURSE_TABLE = './src/models/ta/db2.json';
 const TA_TABLE_TMP = './src/models/ta/db.json.tmp';
@@ -8,59 +9,70 @@ async function addTa(ta, options = { table: TA_TABLE }) {
     (ta = JSON.stringify(ta)), await writer.writeLineToFile(ta, TA_TABLE);
     return true;
 }
+
+async function getAllTas() {
+    const tas = reader.fileAsyncIterator(TA_TABLE);
+    const matchedTas = [];
+    for await (const rawTa of tas) {
+        try {
+            let ta = JSON.parse(rawTa);
+            matchedTas.push(ta);
+        } catch (error) {}
+    }
+
+    return matchedTas;
+}
 async function addCourse(course, options = { table: COURSE_TABLE }) {
-  (course = JSON.stringify(course)), await writer.writeLineToFile(course, COURSE_TABLE);
-  return true;
+    (course = JSON.stringify(course)),
+    await writer.writeLineToFile(course, COURSE_TABLE);
+    return true;
 }
 
 async function deleteTa(TA_name) {
     const tas = reader.fileAsyncIterator(TA_TABLE);
     for await (const rawta of tas) {
-      try {
-        const ta = JSON.parse(rawta);
-        if (ta.TA_name !== TA_name) {
-          await writer.writeLineToFile(rawta, TA_TABLE_TMP);
+        try {
+            const ta = JSON.parse(rawta);
+            if (ta.TA_name !== TA_name) {
+                await writer.writeLineToFile(rawta, TA_TABLE_TMP);
+            }
+        } catch (error) {
+            logger.error({ error, ctx: 'db.core.ta' });
         }
-      } catch (error) {
-        logger.error({ error, ctx: "db.core.ta" });
-      }
     }
     const error = await fs.promises.rename(TA_TABLE_TMP, TA_TABLE);
     if (error) {
-      logger.error({ error, ctx: "db.core.ta" });
-      return false;
+        logger.error({ error, ctx: 'db.core.ta' });
+        return false;
     } else {
-      return true;
+        return true;
     }
-  }
-  
-async function getCourseWithName(searchTerms) {
-  const courses = reader.fileAsyncIterator(COURSE_TABLE);
-  const matchedCourses = [];
-  for await (const rawCourse of courses) {
-    try {
-      let course = JSON.parse(rawCourse);
-      if (
-        Object.entries(searchTerms).every(([key, value]) => {
-          return course[key] === value;
-        })
-      ) {
-        matchedCourses.push(course);
-      }
-    } catch (error) {
-      logger.error({ error, ctx: "db.core.account" });
-    }
-  }
-  return matchedCourses;
 }
 
-
-
-
+async function getCourseWithName(searchTerms) {
+    const courses = reader.fileAsyncIterator(COURSE_TABLE);
+    const matchedCourses = [];
+    for await (const rawCourse of courses) {
+        try {
+            let course = JSON.parse(rawCourse);
+            if (
+                Object.entries(searchTerms).every(([key, value]) => {
+                    return course[key] === value;
+                })
+            ) {
+                matchedCourses.push(course);
+            }
+        } catch (error) {
+            logger.error({ error, ctx: 'db.core.account' });
+        }
+    }
+    return matchedCourses;
+}
 
 module.exports = {
     addTa,
     addCourse,
     deleteTa,
     getCourseWithName,
+    getAllTas,
 };
--- src/routes/ta_administration.js
+++ src/routes/ta_administration.js
@@ -9,9 +9,10 @@ const { checkAuthenticationWithUserType } = require.main.require(
 const fs = require('fs');
 const { addTa } = require.main.require('./src/services/admin');
 const { addCourse } = require.main.require('./src/services/admin');
-const {findTa,deleteTa} = require.main.require("./src/services/admin");
-const {findCourse} = require.main.require("./src/services/course");
-const { response_type } = require.main.require("./src/response");
+const { findTa, deleteTa } = require.main.require('./src/services/admin');
+const { findCourse } = require.main.require('./src/services/course');
+const { response_type } = require.main.require('./src/response');
+const model = require.main.require('./src/models/ta');
 // EXAMPLE OF A GET
 router.get(
     '/',
@@ -39,15 +40,26 @@ router.get(
 
 router.get(
     '/taInfo',
-    checkAuthenticationWithUserType(['admin', 'sysop'], (req, res) => {
+    checkAuthenticationWithUserType(['admin', 'sysop'], async(req, res) => {
+        const tas = await model.getAllTas();
         // res.status(404).json("Not implemented ta administration");
-        res.render('pages/ta_administration/taInfo.ejs');
+        res.render('pages/ta_administration/taInfo.ejs', { tas });
+    })
+);
+router.get(
+    '/taInfo/result',
+    checkAuthenticationWithUserType(['admin', 'sysop'], async(req, res) => {
+        const tas = await model.getAllTas();
+        // console.log(tas)
+        const ta = tas.find((t) => t.TA_name.trim() === req.query.TA.trim());
+        console.log(ta)
+        res.render('pages/ta_administration/ta_result.ejs', { ta });
     })
 );
 
 router.get(
     '/courseInfo',
-    checkAuthenticationWithUserType(['admin', 'sysop'], (req, res) => {
+    checkAuthenticationWithUserType(['admin', 'sysop'], async(req, res) => {
         // res.status(404).json("Not implemented ta administration");
         res.render('pages/ta_administration/courseInfo.ejs');
     })
@@ -61,8 +73,6 @@ router.get(
     })
 );
 
-
-
 //   router.get(
 //     "/courses",
 //     checkAuthenticationWithUserType(["sysop","admin"], async (req, res) => {
@@ -74,7 +84,7 @@ router.get(
 //         });
 //       } else {
 //         const courses = await findCourse(req.query.course_name_search);
-  
+
 //         if (courses.length == 0) {
 //           res.render("pages/ta_administration/edit.ejs", {
 //             userTypes: req.session.user.userTypes,
@@ -92,35 +102,34 @@ router.get(
 //     })
 //   );
 
-  router.get(
-    "/courses",
-    checkAuthenticationWithUserType(["ta","prof"], async (req, res) => {
-      if(!req.query.course_name_search){
-        res.render("pages/ta_administration/edit.ejs", {
-          userTypes: req.session.user.userTypes,
-          username: req.session.user.username,
-          error: "Please enter a search term.",
-        });
-      } else{
-        const courses = await findCourse(req.query.course_name_search);
-  
-        if (courses.length === 0){
-          res.render("pages/ta_administration/edit.ejs", {
-            userTypes: req.session.user.userTypes,
-            username: req.session.user.username,
-            error: "No course matched the search term."
-          });
-        } else{
-          res.render("pages/ta_administration/result.ejs", {
-            userTypes: req.session.user.userTypes,
-            username: req.session.user.username,
-            course: courses[0].course_name,
-          });
+router.get(
+    '/courses',
+    checkAuthenticationWithUserType(['ta', 'prof'], async(req, res) => {
+        if (!req.query.course_name_search) {
+            res.render('pages/ta_administration/edit.ejs', {
+                userTypes: req.session.user.userTypes,
+                username: req.session.user.username,
+                error: 'Please enter a search term.',
+            });
+        } else {
+            const courses = await findCourse(req.query.course_name_search);
+
+            if (courses.length === 0) {
+                res.render('pages/ta_administration/edit.ejs', {
+                    userTypes: req.session.user.userTypes,
+                    username: req.session.user.username,
+                    error: 'No course matched the search term.',
+                });
+            } else {
+                res.render('pages/ta_administration/result.ejs', {
+                    userTypes: req.session.user.userTypes,
+                    username: req.session.user.username,
+                    course: courses[0].course_name,
+                });
+            }
         }
-      } 
     })
-    
-  );
+);
 
 //   router.get(
 //     "/courses",
@@ -133,7 +142,7 @@ router.get(
 //         });
 //       } else{
 //         const courses = await findCourse(req.query.course_name_search);
-  
+
 //         if (courses.length === 0){
 //           res.render("pages/ta_management/ta_management_landing.ejs", {
 //             userTypes: req.session.user.userTypes,
@@ -147,13 +156,9 @@ router.get(
 //             course: courses[0].course_name,
 //           });
 //         }
-//       } 
+//       }
 //     })
 //   );
-
-
-
-
 
 // EXAMPLE OF A POST
 router.post('/hello', async(req, res) => {
@@ -167,22 +172,22 @@ router.post('/hello', async(req, res) => {
             .on('data', function(csvrow) {
                 // console.log(csvrow)
                 const ta = {
-                  term_month_year: csvrow[0],
-                  TA_name: csvrow[1],
-                  student_ID: csvrow[2],
-                  legal_name: csvrow[3],
-                  email: csvrow[4],
-                  grad_ugrad: csvrow[5],
-                  supervisor_name: csvrow[6],
-                  priority: csvrow[7],
-                  hours: csvrow[8],
-                  date_applied: csvrow[9],
-                  location: csvrow[10],
-                  phone: csvrow[11],
-                  degree: csvrow[12],
-                  courses_applied_for: csvrow[13],
-                  open_to_other_courses: csvrow[14],
-                  notes: csvrow[15],
+                    term_month_year: csvrow[0],
+                    TA_name: csvrow[1],
+                    student_ID: csvrow[2],
+                    legal_name: csvrow[3],
+                    email: csvrow[4],
+                    grad_ugrad: csvrow[5],
+                    supervisor_name: csvrow[6],
+                    priority: csvrow[7],
+                    hours: csvrow[8],
+                    date_applied: csvrow[9],
+                    location: csvrow[10],
+                    phone: csvrow[11],
+                    degree: csvrow[12],
+                    courses_applied_for: csvrow[13],
+                    open_to_other_courses: csvrow[14],
+                    notes: csvrow[15],
                 };
                 // console.log(ta);
                 addTa(ta);
@@ -197,8 +202,6 @@ router.post('/hello', async(req, res) => {
     res.render('pages/ta_administration/orangeMenu.ejs');
     // process.stdout.write("Hello World\n");
 });
-
-
 
 // EXAMPLE OF A POST
 router.post('/hello_courses', async(req, res) => {
